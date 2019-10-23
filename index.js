5ce6dfce26c2e2d160f82c921f4c38f6e8bbcf01
ScholliYT <scholliyt@gmail.com>
2019-10-23 02:36:52 +0200

    fixed check for verbose mode value

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
 
-    if(verboseMode) {
+    if(verboseMode.toLowerCase() === 'true') {
         // Get the JSON webhook payload for the event that triggered the workflow
         const payload = JSON.stringify(github.context.payload, undefined, 2)
         console.log(`The event payload: ${payload}`);
