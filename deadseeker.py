f74e3c7ae75d3581bf92a10f142af440ce393eae
Tom Stein <dev@tomstein.me>
2020-11-19 11:02:10 +0100

    default value for exclude_url_prefix

--- deadseeker.py
+++ deadseeker.py
@@ -17,7 +17,7 @@ import backoff
 import logging
 
 search_attrs = set(['href', 'src'])
-excluded_link_prefixes = set(['mailto:'])
+excluded_link_prefixes = set()
 agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
 class LinkParser(HTMLParser):
@@ -107,7 +107,7 @@ verbose = os.environ['INPUT_VERBOSE']
 exclude_prefix = os.environ['INPUT_EXCLUDE_URL_PREFIX']
 print("Checking website: " + str(website_url))
 print("Verbose mode on: " + str(verbose))
-if exclude_prefix is not None:
+if exclude_prefix and exclude_prefix != '':
     for el in exclude_prefix.split(","):
         excluded_link_prefixes.add(el.strip())
     print(f"Excluding prefixes: {excluded_link_prefixes}")
