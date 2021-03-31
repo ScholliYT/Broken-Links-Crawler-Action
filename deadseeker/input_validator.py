b1546aec66dde2aefb00d8e780bae6bcb97e4db7
Adam Eberle <aceberle@gmail.com>
2021-03-31 14:31:27 -0400

    Adding more validation methods

--- deadseeker/input_validator.py
+++ deadseeker/input_validator.py
@@ -1,19 +1,43 @@
 import validators
 import os
-
-
-class ValidatedInput:
-    def __init__(self, website_urls):
-        self.website_urls = website_urls
+from typing import List
 
 
 class InputValidator:
-    def validateInput(self) -> ValidatedInput:
-        website_url = os.environ.get('INPUT_WEBSITE_URL')
-        assert website_url, \
+    def getValidatedUrl(self) -> List[str]:
+        website_urls = self.splitAndTrim('INPUT_WEBSITE_URL')
+        assert website_urls, \
             "'INPUT_WEBSITE_URL' environment variable expected to be provided!"
-        website_urls = [x.strip() for x in website_url.split(',')]
         for url in website_urls:
             assert validators.url(url), \
                 f"Invalid url provided: {url}"
-        return ValidatedInput(website_urls)
+        return website_urls
+
+    def getValidatedVerbosFlag(self) -> bool:
+        verboseStr = os.environ.get('INPUT_VERBOSE') or 'false'
+        verbose = bool(
+            verboseStr and
+            verboseStr.lower() in ['true', 't', 'yes', 'y'])
+        return verbose
+
+    def getValidatedIncludePrefix(self) -> List[str]:
+        return self.splitAndTrim('INPUT_INCLUDE_URL_PREFIX')
+
+    def getValidatedExcludePrefix(self) -> List[str]:
+        return self.splitAndTrim('INPUT_EXCLUDE_URL_PREFIX')
+
+    def getValidatedIncludeSuffix(self) -> List[str]:
+        return self.splitAndTrim('INPUT_INCLUDE_URL_SUFFIX')
+
+    def getValidatedExcludeSuffix(self) -> List[str]:
+        return self.splitAndTrim('INPUT_EXCLUDE_URL_SUFFIX')
+
+    def getValidatedIncludeContained(self) -> List[str]:
+        return self.splitAndTrim('INPUT_INCLUDE_URL_CONTAINED')
+
+    def getValidatedExcludeContained(self) -> List[str]:
+        return self.splitAndTrim('INPUT_EXCLUDE_URL_CONTAINED')
+
+    def splitAndTrim(self, name) -> List[str]:
+        valueStr = os.environ.get(name)
+        return [] if not valueStr else [x.strip() for x in valueStr.split(',')]
--- test/test_input_validator.py
+++ test/test_input_validator.py
@@ -1,7 +1,17 @@
-from deadseeker.input_validator import InputValidator, ValidatedInput
+from deadseeker.input_validator import InputValidator
 import os
 import unittest
 from unittest.mock import patch
+from typing import Dict
+
+INCLUDE_EXCLUDE_METHODS_BY_VARNAME: Dict[str, str] = {
+    'INPUT_INCLUDE_URL_PREFIX': 'getValidatedIncludePrefix',
+    'INPUT_EXCLUDE_URL_PREFIX': 'getValidatedExcludePrefix',
+    'INPUT_INCLUDE_URL_SUFFIX': 'getValidatedIncludeSuffix',
+    'INPUT_EXCLUDE_URL_SUFFIX': 'getValidatedExcludeSuffix',
+    'INPUT_INCLUDE_URL_CONTAINED': 'getValidatedIncludeContained',
+    'INPUT_EXCLUDE_URL_CONTAINED': 'getValidatedExcludeContained',
+}
 
 
 class TestInputValidator(unittest.TestCase):
@@ -12,38 +22,83 @@ class TestInputValidator(unittest.TestCase):
 
     def test_emptyUrlThrowsException(self):
         with self.assertRaises(Exception) as context:
-            self.validateInput()
+            self.testObj.getValidatedUrl()
         self.assertTrue(
             '\'INPUT_WEBSITE_URL\' environment' +
             ' variable expected to be provided!'
             in str(context.exception))
 
+    @patch.dict(os.environ, {'INPUT_WEBSITE_URL': 'bananas'})
     def test_badUrlThrowsException(self):
-        self.env['INPUT_WEBSITE_URL'] = 'bananas'
         with self.assertRaises(Exception) as context:
-            self.validateInput()
+            self.testObj.getValidatedUrl()
         self.assertTrue(
             'Invalid url provided: bananas'
             in str(context.exception))
 
+    @patch.dict(os.environ, {'INPUT_WEBSITE_URL': 'https://www.google.com'})
     def test_goodUrlReturned(self):
-        url = 'https://www.google.com'
-        self.env['INPUT_WEBSITE_URL'] = url
-        self.validateInput()
-        self.assertEqual(1, len(self.result.website_urls))
-        self.assertTrue(url in self.result.website_urls)
+        self.testObj.getValidatedUrl()
+        urls = self.testObj.getValidatedUrl()
+        self.assertEqual(1, len(urls))
+        self.assertTrue('https://www.google.com' in urls)
 
+    @patch.dict(os.environ,
+                {'INPUT_WEBSITE_URL':
+                    'https://www.google.com,https://www.apple.com'})
     def test_multipleGoodUrlsReturned(self):
-        url = 'https://www.google.com,https://www.apple.com'
-        self.env['INPUT_WEBSITE_URL'] = url
-        self.validateInput()
-        self.assertEqual(2, len(self.result.website_urls))
-        self.assertTrue('https://www.google.com' in self.result.website_urls)
-        self.assertTrue('https://www.apple.com' in self.result.website_urls)
+        urls = self.testObj.getValidatedUrl()
+        self.assertEqual(2, len(urls))
+        self.assertTrue('https://www.google.com' in urls)
+        self.assertTrue('https://www.apple.com' in urls)
 
-    def validateInput(self):
-        with patch.dict(os.environ, self.env):
-            self.result = self.testObj.validateInput()
+    def test_verboseFalseByDefault(self):
+        self.assertFalse(
+            self.testObj.getValidatedVerbosFlag(),
+            'Expected default value for verbos to be False')
+
+    def test_verboseTrueWhenTrue(self):
+        for verboseStr in [
+                'true', 't', 'yes', 'y', 'True',
+                'T', 'Yes', 'Y', 'TRUE', 'YES']:
+            with patch.dict(
+                    os.environ,
+                    {'INPUT_VERBOSE': verboseStr}):
+                self.assertTrue(
+                    self.testObj.getValidatedVerbosFlag(),
+                    'Expected value to evaluate to' +
+                    f' verbose true: {verboseStr}')
+
+    def test_verboseFalseWhenFalse(self):
+        for verboseStr in [
+                'false', 'f', 'no', 'n', 'False',
+                'F', 'No', 'N', 'FALSE', 'NO']:
+            with patch.dict(
+                    os.environ,
+                    {'INPUT_VERBOSE': verboseStr}):
+                self.assertFalse(
+                    self.testObj.getValidatedVerbosFlag(),
+                    'Expected value to evaluate to' +
+                    f' verbose false: {verboseStr}')
+
+    def test_defaultIncludeExcludeValueIsEmpty(self):
+        for methodName in INCLUDE_EXCLUDE_METHODS_BY_VARNAME.values():
+            method = getattr(self.testObj, methodName)
+            results = method()
+            self.assertFalse(
+                results,
+                f'Expected method named {methodName} to return empty list, ' +
+                'but instead returned: {results}')
+
+    def test_expectedIncludeExcludeValues(self):
+        for varName, methodName in INCLUDE_EXCLUDE_METHODS_BY_VARNAME.items():
+            method = getattr(self.testObj, methodName)
+            with patch.dict(
+                    os.environ,
+                    {varName: 'a,b,c,d,e'}):
+                results = method()
+                self.assertEqual(
+                    5, len(results))
 
 
 if __name__ == '__main__':
