fda1d42b0c8dfe95adbf6b616491c2ffc3f13140
Adam Eberle <aceberle@gmail.com>
2021-04-02 16:40:05 -0400

    Adding enhanced log checks

--- test/test_deadseeker.py
+++ test/test_deadseeker.py
@@ -9,7 +9,7 @@ from deadseeker.linkacceptor import LinkAcceptorBuilder
 import socket
 from threading import Thread
 import os
-from typing import ClassVar
+from typing import ClassVar, List
 from logging import DEBUG
 import logging
 
@@ -61,22 +61,63 @@ class TestDeadSeeker(unittest.TestCase):
         cls.mock_server_thread = Thread(target=cls.mock_server.serve_forever)
         cls.mock_server_thread.setDaemon(True)
         cls.mock_server_thread.start()
-        cls.url = f'http://localhost:{port}/'
+        cls.url = f'http://localhost:{port}'
 
     def setUp(self):
         self.config = DeadSeekerConfig()
+        self.logger = logging.getLogger('deadseeker.deadseeker')
+        self.builder = LinkAcceptorBuilder()
 
-    def test_request_response(self):
-        self.config.linkacceptor = \
-            LinkAcceptorBuilder().addExcludePrefix(
-                    'https://www.google.com').build()
+    def test_numberFailedIs2(self):
+        self.builder.addExcludePrefix('https://www.google.com')
+        response = self._seek_with_logging()
+        actualfailed = len(response.failures)
+        self.assertEqual(2, actualfailed)
+
+    def test_numberFailedIs0WithMoreExcludes(self):
+        self.builder.addExcludePrefix(
+            'https://www.google.com',
+            '/page3.html',
+            '/page4.html')
+        response = self._seek_with_logging()
+        actualfailed = len(response.failures)
+        self.assertEqual(0, actualfailed)
+
+    def test_messagesLogged(self):
+        self.builder.addExcludePrefix('https://www.google.com')
+        with patch.object(self.logger, 'error') as error_mock,\
+                patch.object(self.logger, 'info') as info_mock:
+            self._seek_with_logging()
+            expected_errors = [
+                f'::error ::ClientResponseError: 500 - {self.url}/page4.html',
+                f'::error ::ClientResponseError: 404 - {self.url}/page3.html'
+            ]
+            actual_errors: List[str] = []
+            for call in error_mock.call_args_list:
+                args, kwargs = call
+                actual_errors.append(args[0])
+            self.assertEqual(expected_errors, actual_errors)
+
+            expected_info_prefixes = [
+                f'200 - {self.url} - ',
+                f'200 - {self.url}/page1.html - ',
+                f'200 - {self.url}/page2.html - '
+            ]
+            actual_infos: List[str] = []
+            for call in info_mock.call_args_list:
+                args, kwargs = call
+                actual_infos.append(args[0])
+            self.assertEqual(len(expected_info_prefixes), len(actual_infos))
+            for expected_prefix, actual \
+                    in zip(expected_info_prefixes, actual_infos):
+                self.assertTrue(actual.startswith(expected_prefix))
+
+    def _seek_with_logging(self):
+        self.config.linkacceptor = self.builder.build()
         seeker = DeadSeeker(self.config)
-        logger = logging.getLogger('deadseeker.deadseeker')
-        with patch.object(logger, 'getEffectiveLevel') as log_level:
+        with patch.object(self.logger, 'getEffectiveLevel') as log_level:
             log_level.return_value = DEBUG
-            response = seeker.seek([self.url])
-            actualfailed = len(response.failures)
-            self.assertEqual(2, actualfailed)
+            return seeker.seek([self.url])
 
 
 if __name__ == '__main__':
