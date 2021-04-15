6670748dd3804f3154fcfe8451e918919dba3bab
Adam Eberle <aceberle@gmail.com>
2021-04-15 10:35:03 -0400

    Adding missing return types

--- deadseeker/linkacceptor.py
+++ deadseeker/linkacceptor.py
@@ -58,34 +58,34 @@ class LinkAcceptorBuilder:
     def __init__(self) -> None:
         self.acceptors: List[LinkAcceptor] = []
 
-    def addIncludePrefix(self, *args: str):
+    def addIncludePrefix(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(IncludePrefixLinkAcceptor(list(args)))
         return self
 
-    def addExcludePrefix(self, *args: str):
+    def addExcludePrefix(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(NotLinkAcceptor(
                 IncludePrefixLinkAcceptor(list(args))))
         return self
 
-    def addIncludeSuffix(self, *args: str):
+    def addIncludeSuffix(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(IncludeSuffixLinkAcceptor(list(args)))
         return self
 
-    def addExcludeSuffix(self, *args: str):
+    def addExcludeSuffix(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(NotLinkAcceptor(
                 IncludeSuffixLinkAcceptor(list(args))))
         return self
 
-    def addIncludeContained(self, *args: str):
+    def addIncludeContained(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(IncludeContainedLinkAcceptor(list(args)))
         return self
 
-    def addExcludeContained(self, *args: str):
+    def addExcludeContained(self, *args: str) -> 'LinkAcceptorBuilder':
         if args:
             self.acceptors.append(NotLinkAcceptor(
                 IncludeContainedLinkAcceptor(list(args))))
