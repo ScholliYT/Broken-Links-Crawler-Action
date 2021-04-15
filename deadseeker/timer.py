b0203fabfe4eb3f77fce6ebcc5574069074710e5
Adam Eberle <aceberle@gmail.com>
2021-04-15 10:10:04 -0400

    Added missing return types

--- deadseeker/timer.py
+++ deadseeker/timer.py
@@ -2,11 +2,11 @@ import time
 
 
 class Timer:
-    def __init__(self):
+    def __init__(self) -> None:
         self.start: float = time.time()
         self.end: float = -1
 
-    def stop(self):
+    def stop(self) -> float:
         if(self.end < 0):
             self.end = time.time()
         return self.end - self.start
