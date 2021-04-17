b04c3f9a2c6a7eca1bcdbb9c42a5b367a7998a81
Adam Eberle <aceberle@gmail.com>
2021-04-17 11:41:40 -0400

    Adding asyncmock

---
+++ test/asyncmock.py
@@ -0,0 +1,16 @@
+from unittest import Mock
+
+
+# The normal Mock class doesn't specifically declare
+# the expected functions provided by an asynchronous
+# context manager, so we have to declare a special class
+# for the python code to recognize the object as an
+# async context manager.
+# Special thanks to PAWEŁ FERTYK for this one:
+# https://pfertyk.me/2017/06/testing-asynchronous-context-managers-in-python/
+class AsyncContextManagerMock(Mock):
+    async def __aenter__(self):
+        return self.aenter
+
+    async def __aexit__(self, *args):
+        pass
