b2f402cc76e6587c974649dc872cbed407c3971c
Adam Eberle <aceberle@gmail.com>
2021-04-19 16:39:32 -0400

    Making api changes and adding unit test

--- deadseeker/deadseeker.py
+++ deadseeker/deadseeker.py
@@ -1,16 +1,28 @@
 import asyncio
-import aiohttp
 from urllib.parse import urlparse, urljoin
-from .linkparser import LinkParser
-from typing import List, Set, Deque
-import time
+from typing import List, Set, Deque, Optional
+from .timer import Timer
 import logging
-from .clientsessionfactory import ClientSessionFactory
+from .clientsession import ClientSessionFactory, DefaultClientSessionFactory
 from .common import (
     SeekerConfig,
-    UrlFetchResponse,
     UrlTarget,
-    SeekResults
+    SeekResults,
+    UrlFetchResponse,
+    UrlFetchResponseHandler
+)
+from .responsefetcher import (
+    ResponseFetcherFactory,
+    DefaultResponseFetcherFactory
+)
+from .linkacceptor import (
+    LinkAcceptor,
+    LinkAcceptorFactory,
+    DefaultLinkAcceptorFactory
+)
+from .linkparser import (
+    LinkParserFactory,
+    DefaultLinkParserFactory
 )
 
 logger = logging.getLogger(__name__)
@@ -19,46 +31,32 @@ logger = logging.getLogger(__name__)
 class DeadSeeker:
     def __init__(self, config: SeekerConfig) -> None:
         self.config = config
-        self.clientSessionFactory = ClientSessionFactory()
+        self.clientsession: ClientSessionFactory =\
+            DefaultClientSessionFactory()
+        self.responsefetcherfactory: ResponseFetcherFactory =\
+            DefaultResponseFetcherFactory()
+        self.linkacceptorfactory: LinkAcceptorFactory =\
+            DefaultLinkAcceptorFactory()
+        self.linkparserfactory: LinkParserFactory =\
+            DefaultLinkParserFactory()
 
-    async def _get_urlfetchresponse(
+    async def _main(
             self,
-            session: aiohttp.ClientSession,
-            urltarget: UrlTarget) -> UrlFetchResponse:
-        resp = UrlFetchResponse(urltarget)
-        start = time.time()  # measure load time (HEAD only)
-        url = urltarget.url
-        end: float = -1
-        try:
-            async with session.head(url) as headresponse:
-                end = time.time()
-                resp.status = headresponse.status
-                has_html = 'html' in headresponse.headers['Content-Type']
-                onsite = urltarget.home in url
-                if(has_html and onsite):
-                    async with session.get(url) as getresponse:
-                        resp.html = await getresponse.text()
-        except aiohttp.ClientResponseError as e:
-            resp.status = e.status
-            resp.error = e
-        except aiohttp.ClientError as e:
-            resp.status = -1
-            resp.error = e
-        if end < 0:
-            end = time.time()
-        resp.elapsed = (end - start)*1000
-        return resp
-
-    async def _main(self, urls: List[str]) -> SeekResults:
-        start = time.time()
+            urls: List[str],
+            responsehandler: Optional[UrlFetchResponseHandler] = None
+            ) -> SeekResults:
+        timer = Timer()
         results = SeekResults()
         visited: Set[str] = set()
-        targets: Deque[UrlTarget] = Deque()
+        targets: Deque[UrlTarget] = Deque[UrlTarget]()
         for url in urls:
             visited.add(url)
             targets.appendleft(UrlTarget(url, url, self.config.max_depth))
-        linkparser = LinkParser(self.config.linkacceptor)
-        async with self.clientSessionFactory.createClientSession(
+        linkacceptor = self.linkacceptorfactory.get_link_acceptor(self.config)
+        linkparser = self.linkparserfactory.get_link_parser(linkacceptor)
+        responsefetcher = self.responsefetcherfactory.get_response_fetcher(
+                                self.config)
+        async with self.clientsession.get_client_session(
                 self.config) as session:
             while targets:
                 tasks = []
@@ -66,32 +64,44 @@ class DeadSeeker:
                     urltarget = targets.pop()
                     tasks.append(
                         asyncio.create_task(
-                            self._get_urlfetchresponse(session, urltarget)))
+                            responsefetcher.fetch_response(
+                                session, urltarget)))
                 for task in asyncio.as_completed(tasks):  # completed first
                     resp = await task
-                    self.config.responsehandler.handle_response(resp)
-                    if(resp.error):
+                    if responsehandler:
+                        responsehandler.handle_response(resp)
+                    if resp.error:
                         results.failures.append(resp)
                     else:
                         results.successes.append(resp)
-                    url = resp.urltarget.url
-                    depth = resp.urltarget.depth
-                    if(resp.html and depth != 0):
-                        home = resp.urltarget.home
-                        linkparser.reset()
-                        linkparser.feed(resp.html)
-                        for newurl in linkparser.links:
-                            if not bool(
-                                    urlparse(newurl).netloc):  # relative link?
-                                newurl = urljoin(url, newurl)
-                            if newurl not in visited:
-                                visited.add(newurl)
-                                targets.appendleft(
-                                    UrlTarget(home, newurl, depth - 1))
-        results.elapsed = (time.time() - start) * 1000
+                    self._parse_response(visited, targets, linkparser, resp)
+        results.elapsed = timer.stop() * 1000
         return results
 
-    def seek(self, urls: List[str]) -> SeekResults:
-        results = asyncio.run(self._main(urls))
+    def _parse_response(
+            self,
+            visited: Set[str],
+            targets: Deque[UrlTarget],
+            linkparser: LinkAcceptor,
+            resp: UrlFetchResponse) -> None:
+        depth = resp.urltarget.depth
+        if resp.html and depth != 0:
+            home = resp.urltarget.home
+            links = linkparser.parse(resp.html)
+            for newurl in links:
+                if not bool(
+                        urlparse(newurl).netloc):  # relative link?
+                    newurl = urljoin(resp.urltarget.url, newurl)
+                if newurl not in visited:
+                    visited.add(newurl)
+                    targets.appendleft(
+                        UrlTarget(home, newurl, depth - 1))
+
+    def seek(
+            self,
+            urls: List[str],
+            responsehandler: UrlFetchResponseHandler = None) -> SeekResults:
+        url_list = [urls] if isinstance(urls, str) else urls
+        results = asyncio.run(self._main(url_list, responsehandler))
         logger.debug(f'Process took {results.elapsed:.2f}')
         return results
