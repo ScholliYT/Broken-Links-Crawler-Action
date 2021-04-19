6f91fd6124d2ecbfd4a6837110e171814b4d058c
Adam Eberle <aceberle@gmail.com>
2021-04-19 16:36:07 -0400

    Removing unnecessary level check and enhancing unit test

--- test/test_loggingresponsehandler.py
+++ test/test_loggingresponsehandler.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 from deadseeker.common import UrlFetchResponse, UrlTarget
 from deadseeker.loggingresponsehandler import LoggingUrlFetchResponseHandler
-from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 from aiohttp import ClientError
 import logging
 
@@ -18,66 +17,47 @@ class TestLoggingResponseHandler(unittest.TestCase):
         self.resp.elapsed = 1234.1234
         self.logger = logging.getLogger('deadseeker.loggingresponsehandler')
 
-    def test_no_logs_when_level_over_info(self):
-        for level in [WARNING, ERROR, CRITICAL]:
-            with patch.object(self.logger, 'error') as error_mock,\
-                    patch.object(self.logger, 'info') as info_mock:
-                self.logger.setLevel(level)
-                self.testobj.handle_response(self.resp)
-                self.assertFalse(
-                    info_mock.called,
-                    f'Did not expect any logs with level set to: {level}')
-                self.assertFalse(
-                    error_mock.called,
-                    f'Did not expect any logs with level set to: {level}')
-
-    def test_info_logs_when_level_at_or_under_info_and_success(self):
+    def test_info_logs_when_success(self):
         self.resp.status = 200
-        for level in [DEBUG, INFO]:
-            with patch.object(self.logger, 'error') as error_mock,\
-                    patch.object(self.logger, 'info') as info_mock:
-                self.logger.setLevel(level)
-                self.testobj.handle_response(self.resp)
-                expected = '200 - http://testing.test.com/ - 1234.12ms'
-                self.assertTrue(
-                    info_mock.called_with_args(expected),
-                    f'Expected info message to be logged: {expected}')
-                self.assertFalse(
-                    error_mock.called,
-                    'Did not expect any error logs!')
+        with patch.object(self.logger, 'error') as error_mock,\
+                patch.object(self.logger, 'info') as info_mock:
+            self.testobj.handle_response(self.resp)
+            expected = '200 - http://testing.test.com/ - 1234.12ms'
+            self.assertTrue(
+                info_mock.called_with_args(expected),
+                f'Expected info message to be logged: {expected}')
+            self.assertFalse(
+                error_mock.called,
+                'Did not expect any error logs!')
 
-    def test_error_logs_when_level_at_or_under_info_and_responseerror(self):
+    def test_error_logs_when_responseerror(self):
         self.resp.status = 400
         self.resp.error = ClientError()
-        for level in [DEBUG, INFO]:
-            with patch.object(self.logger, 'error') as error_mock,\
-                    patch.object(self.logger, 'info') as info_mock:
-                self.logger.setLevel(level)
-                self.testobj.handle_response(self.resp)
-                expected = '::error ::ClientError: 400' +\
-                    ' - http://testing.test.com/'
-                self.assertTrue(
-                    error_mock.called_with_args(expected),
-                    f'Expected error message to be logged: {expected}')
-                self.assertFalse(
-                    info_mock.called,
-                    'Did not expect any info logs!')
+        with patch.object(self.logger, 'error') as error_mock,\
+                patch.object(self.logger, 'info') as info_mock:
+            self.testobj.handle_response(self.resp)
+            expected = '::error ::ClientError: 400' +\
+                ' - http://testing.test.com/'
+            self.assertTrue(
+                error_mock.called_with_args(expected),
+                f'Expected error message to be logged: {expected}')
+            self.assertFalse(
+                info_mock.called,
+                'Did not expect any info logs!')
 
-    def test_error_logs_when_level_at_or_under_info_and_no_status_error(self):
+    def test_error_logs_when_no_status_error(self):
         self.resp.error = ClientError('test error')
-        for level in [DEBUG, INFO]:
-            with patch.object(self.logger, 'error') as error_mock,\
-                    patch.object(self.logger, 'info') as info_mock:
-                self.logger.setLevel(level)
-                self.testobj.handle_response(self.resp)
-                expected = '::error ::ClientError: test error' +\
-                    ' - http://testing.test.com/'
-                self.assertTrue(
-                    error_mock.called_with_args(expected),
-                    f'Expected error message to be logged: {expected}')
-                self.assertFalse(
-                    info_mock.called,
-                    'Did not expect any info logs!')
+        with patch.object(self.logger, 'error') as error_mock,\
+                patch.object(self.logger, 'info') as info_mock:
+            self.testobj.handle_response(self.resp)
+            expected = '::error ::ClientError: test error' +\
+                ' - http://testing.test.com/'
+            self.assertTrue(
+                error_mock.called_with_args(expected),
+                f'Expected error message to be logged: {expected}')
+            self.assertFalse(
+                info_mock.called,
+                'Did not expect any info logs!')
 
 
 if __name__ == '__main__':
