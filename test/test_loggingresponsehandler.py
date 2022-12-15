647a0a6e79377e8be9f0d7cac62bc23e58dfe466
Tom Stein <dev@tomstein.me>
2022-12-15 20:34:35 +0000

    add unit test for navigation path

--- test/test_loggingresponsehandler.py
+++ test/test_loggingresponsehandler.py
@@ -6,6 +6,7 @@ from aiohttp import ClientError
 import logging
 
 TEST_URL = 'http://testing.test.com/'
+TEST_SUBPAGE_URL = 'http://subpage.testing.test.com/'
 
 
 class TestLoggingResponseHandler(unittest.TestCase):
@@ -13,8 +14,14 @@ class TestLoggingResponseHandler(unittest.TestCase):
     def setUp(self):
         self.testobj = LoggingUrlFetchResponseHandler()
         target = UrlTarget(TEST_URL, TEST_URL, 0)
+
         self.resp = UrlFetchResponse(target)
         self.resp.elapsed = 1234.1234
+
+        subpage_target = target.child(TEST_SUBPAGE_URL).child(TEST_SUBPAGE_URL + 'page1')
+        self.subpage_response = UrlFetchResponse(subpage_target)
+        self.subpage_response.elapsed = 12.12
+
         self.logger = logging.getLogger('deadseeker.loggingresponsehandler')
 
     def test_info_logs_when_success(self):
@@ -39,6 +46,23 @@ class TestLoggingResponseHandler(unittest.TestCase):
             error_mock.assert_called_with(expected_error)
             debug_mock.assert_called_with(expected_debug,
                                           exc_info=self.resp.error)
+            info_mock.assert_not_called()
+
+    def test_error_logs_when_responseerror_including_navigationpath(self):
+        self.subpage_response.status = 400
+        self.subpage_response.error = ClientError()
+        with patch.object(self.logger, 'error') as error_mock,\
+                patch.object(self.logger, 'info') as info_mock,\
+                patch.object(self.logger, 'debug') as debug_mock:
+            self.testobj.handle_response(self.subpage_response)
+            expected_error = '::error ::ClientError: 400' +\
+                ' - http://subpage.testing.test.com/page1' +\
+                '. Found by navigating through: http://testing.test.com/ ' +\
+                '-> http://subpage.testing.test.com/.'
+            expected_debug = 'The following exception occured'
+            error_mock.assert_called_with(expected_error)
+            debug_mock.assert_called_with(expected_debug,
+                                          exc_info=self.subpage_response.error)
             info_mock.assert_not_called()
 
     def test_error_logs_when_no_status_error(self):
